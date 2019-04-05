35bcf15175eafeeb9b9d573a9a301f35f080dd9c
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-05 10:21:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#JAVASCRIPT EXERCISE
+
+This is the 5th Javascript exercise of Impact byte Haku
---
+++ index.js
@@ -0,0 +1,17 @@
+console.log([1, 2, 3]);
+console.log([0, 1, 2, 3]);
+console.log(["Alpha", 100, true]);
+
+const person = ["Alpha", 100, true, false];
+
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+console.log(person[3]);
+
+alert(person);
+alert(person[0]);
+alert(person[1]);
+alert(person[2]);
+alert(person[3]);
